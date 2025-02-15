8ae7daf2c1ce7c5dea2de9da1bad110746c3e040
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2025-02-16 00:02:52 +0330

    Make emojis use a different font

--- config.h
+++ config.h
@@ -9,6 +9,7 @@ static char *font = "caskaydiaCove NF:pixelsize=16:antialias=true:autohint=true:
 #if FONT2_PATCH
 /* Spare fonts */
 static char *font2[] = {
+"noto color emoji:style=regular",
 "joyPixels:pixelsize=16:antialias=true:autohint=true",
 "FontAwesome:pixelsize=16:antialias=true:autohint=true",
 "Noto Sans CJK JP:pixelsize=16:style=regular:antialias=true:autohint=true",
